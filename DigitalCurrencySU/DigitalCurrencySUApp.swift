a44ab5f6b0be242a993b80b7b5d5e7c5c2bcc56a
Erick Arias <erick.arias.barrios@gmail.com>
2023-04-04 16:48:32 -0500

    Initial Commit

---
+++ DigitalCurrencySU/DigitalCurrencySUApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalCurrencySUApp.swift
+//  DigitalCurrencySU
+//
+//  Created by Erick Arias on 4/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DigitalCurrencySUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
